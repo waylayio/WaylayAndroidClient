b007bf534923e248fd82895803b4a520ce33e5d2
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-01-15 14:26:04 +0100

    location manager fix

--- src/waylay/client/activity/MainActivity.java
+++ src/waylay/client/activity/MainActivity.java
@@ -110,11 +110,16 @@ public class MainActivity extends TabActivity implements LocationListener, Senso
 
 	//sensors
 	protected LocationSensor locationSensor = new LocationSensor();
+	protected Criteria criteria = new Criteria();
+	// The minimum distance to change Updates in meters
+	public static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10
+	// The minimum time between updates in milliseconds
+	public static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1; // 1 minute
 	protected AccelerometerSensor accelometerSensor = new AccelerometerSensor();
 	protected ForceSensor velocitySensor = new ForceSensor();
 	private LocationManager locationManager;
 	private SensorManager sensorManager;
-	private String provider;
+	private String provider = LocationManager.NETWORK_PROVIDER;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -216,13 +221,55 @@ public class MainActivity extends TabActivity implements LocationListener, Senso
 			}
 		});  
 	    
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    // Define the criteria how to select the locatioin provider -> use
-	    // default
-	    Criteria criteria = new Criteria();
-	    provider = locationManager.getBestProvider(criteria, false);
-	    Location location = locationManager.getLastKnownLocation(provider);
+		
+	    //
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
 
+        // getting GPS status
+        boolean isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // getting network status
+        boolean isNetworkEnabled = locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        Location location = null;
+
+
+        if (!isGPSEnabled && !isNetworkEnabled) {
+            // no network provider is enabled
+        } else {
+            // First get location from Network Provider
+            if (isNetworkEnabled) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                Log.d("Network", "Network");
+                if (locationManager != null) {
+                    location = locationManager
+                            .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    provider = LocationManager.NETWORK_PROVIDER;
+                }
+            }
+            // if GPS Enabled get lat/long using GPS Services
+            if (isGPSEnabled) {
+                if (location == null) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("GPS Enabled", "GPS Enabled");
+                    if (locationManager != null) {
+                        location = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        provider = LocationManager.GPS_PROVIDER;
+                    }
+                }
+            }
+        }
+
+	 
 	    // Initialize the location fields
 	    if (location != null) {
 	      Log.d(TAG, "Provider " + provider + " has been selected.");
@@ -230,6 +277,7 @@ public class MainActivity extends TabActivity implements LocationListener, Senso
 	    } else {
 	      Log.w(TAG, "Location not available");
 	    }
+        
 	    sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 	    sensorManager.registerListener(this,
 	            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
@@ -919,7 +967,10 @@ public class MainActivity extends TabActivity implements LocationListener, Senso
 	  @Override
 	  protected void onResume() {
 	    super.onResume();
-	    locationManager.requestLocationUpdates(provider, 400, 1, this);
+	    locationManager.requestLocationUpdates(
+                provider,
+                MIN_TIME_BW_UPDATES,
+                MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 	    sensorManager.registerListener(this,
 	            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
 	    sensorManager.registerListener(this,
